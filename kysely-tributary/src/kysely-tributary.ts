ea39c4fa85a1287ffa4a27a557112c9fb618851b
Goose + Frank Murphy <frank@wallaroo.ai>
2025-10-29 17:03:19 +0000

    Fix KysleyTributary types

--- kysely-tributary/src/kysely-tributary.ts
+++ kysely-tributary/src/kysely-tributary.ts
@@ -1,4 +1,4 @@
-import { TributaryClient, TributaryStream } from 'tributary-client'
+import { TributaryClient, TributaryStream, TributaryLocal } from 'tributary-client'
 import { 
   CompiledQuery,
   DatabaseConnection,
@@ -12,14 +12,17 @@ import {
 } from 'kysely'
 import { TributaryDriver } from './tributary-driver.js'
 
+// Type for objects that can be used with TributaryDriver
+type TributaryQueryable = TributaryStream | TributaryLocal;
+
 export class KyselyTributary {
-  stream: TributaryStream
+  stream: TributaryQueryable
 
   /**
    * Create a new KyselyTributary instance.
-   * @param stream The TributaryStream instance to use for database operations
+   * @param stream The TributaryStream or TributaryLocal instance to use for database operations
    */
-  constructor(stream: TributaryStream) {
+  constructor(stream: TributaryQueryable) {
     this.stream = stream
   }
 
